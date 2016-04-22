c5bb0376febecc1e295c256519b70c1f7c8700a8
Daniel Krainas <dkrainas87@gmail.com>
2016-04-21 22:30:28 -0400

    use reflection to bind env vars to config

--- config/config.go
+++ config/config.go
@@ -1,13 +1,17 @@
 package config
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"reflect"
+	"strings"
 
 	"github.com/danielkrainas/canaria-api/logging"
 )
 
 type StorageConfig struct {
-	Driver string
+	Driver string `config_default:"true"`
 }
 
 type Config struct {
@@ -32,13 +36,69 @@ func validKey(key string, value string) bool {
 
 func LoadConfig() (*Config, error) {
 	config := newConfig()
+	err := config.BindEnv()
+	if err == nil {
+		err = config.Validate()
+	}
 
-	if config.Storage != nil {
-		driverName := os.Getenv("CANARY_STORAGE")
-		if validKey("CANARY_STORAGE", driverName) {
-			config.Storage.Driver = driverName
+	return config, err
+}
+
+func (c *Config) BindEnv() error {
+	return mapEnvToObject(c, "CANARY")
+}
+
+func (c *Config) Validate() error {
+	if c.Storage != nil {
+		if c.Storage.Driver == "" {
+			return errors.New("no storage driver selected")
 		}
 	}
 
-	return config, nil
+	return nil
+}
+
+func mapEnvToObject(data interface{}, path string) error {
+	v := reflect.ValueOf(data)
+	t := v.Type()
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+		v = v.Elem()
+	}
+
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		fv := v.Field(i)
+		ft := fv.Type()
+
+		if ft.Kind() == reflect.Ptr {
+			ft = ft.Elem()
+		}
+
+		key := fmt.Sprintf("%s_%s", path, strings.ToUpper(f.Name))
+		if ft.Kind() != reflect.Struct && strings.ToLower(f.Tag.Get("config_default")) == "true" {
+			key = path
+		}
+
+		switch ft.Kind() {
+		case reflect.Struct:
+			err := mapEnvToObject(fv.Interface(), key)
+			if err != nil {
+				return err
+			}
+
+			break
+
+		case reflect.String:
+			logging.Trace.Println(key)
+			fv.SetString(os.Getenv(key))
+			break
+
+		default:
+			logging.Warning.Printf("unsupported type for key \"%s\"", key)
+			break
+		}
+	}
+
+	return nil
 }

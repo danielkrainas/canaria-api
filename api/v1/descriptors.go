a03a0ee402a9c82702f09157f133ae4310d03f4f
Danny Krainas <dkrainas87@gmail.com>
2016-05-26 13:47:12 -0400

    fix uuid regex

--- api/v1/descriptors.go
+++ api/v1/descriptors.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	IdRegex = regexp.MustCompile(`(?i)[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}\\Z`)
+	IdRegex = regexp.MustCompile(`(?i)[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}`)
 
 	uuidParameter = describe.ParameterDescriptor{
 		Name:        "uuid",

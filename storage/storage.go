450e6bf5e44d77aea49fc075ad0e11b8699dc622
Danny Krainas <dkrainas87@gmail.com>
2016-05-28 01:18:43 -0400

    wip: breakout webhooks from canaries

--- storage/storage.go
+++ storage/storage.go
@@ -8,10 +8,23 @@ import (
 )
 
 type StorageDriver interface {
+	Canaries() CanaryStorage
+	Hooks() HookStorage
+}
+
+type CanaryStorage interface {
 	IsDeleted(ctx context.Context, id string) bool
 	Get(ctx context.Context, id string) (*common.Canary, error)
-	Save(ctx context.Context, c *common.Canary) error
+	Store(ctx context.Context, c *common.Canary) error
 	Delete(ctx context.Context, id string) error
+}
+
+type HookStorage interface {
+	Get(ctx context.Context, id string) (*common.WebHook, error)
+	Store(ctx context.Context, h *common.WebHook) error
+	Delete(ctx context.Context, id string) error
+	GetForCanary(ctx context.Context, canaryID string) ([]*common.WebHook, error)
+	DeleteForCanary(ctx context.Context, canaryID string) ([]string, error)
 }
 
 type Error struct {
